b2ac2af2c6cf1e2c6bab369d228d5a1a8693de59
DuyKhanhNguyen <khanhnguyenduy.cv@gmail.com>
2024-11-20 20:17:48 +0700

    push_front and push_back function

--- link_list.c
+++ link_list.c
@@ -13,6 +13,15 @@ typedef struct List {
     size_t size;
 } List;
 
+Node *get_end_node(List *list) {
+    if (list->head == NULL) return NULL;
+    Node *current = list->head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    return current;
+}
+
 List *init_empty_list() {
     List *list = (List *)malloc(sizeof(List));
     if (list == NULL) {
@@ -77,11 +86,50 @@ void *back(List *list) {
         printf("Link List Empty.\n");
         return NULL;
     }
-    Node *currency = list->head;
-    while (currency->next != NULL) {
-        currency = currency->next;
+    Node *current = list->head;
+    while (current->next != NULL) {
+        current = current->next;
     }
-    return currency->data;
+    return current->data;
+}
+
+void push_front(List *list, void* val, size_t data_size) {
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    if (new_node == NULL) {
+        perror("Cannot allocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+    new_node->data = malloc(data_size);
+    if (new_node->data == NULL) {
+        perror("Cannot allocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(new_node->data, val, data_size);
+    new_node->next = list->head;
+    list->head = new_node;
+    list->size++;
+}
+
+void push_back(List *list, void* val, size_t data_size) {
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    if (new_node == NULL) {
+        perror("Cannot allocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+    new_node->data = malloc(data_size);
+    if(new_node->data == NULL) {
+        perror("Cannot allocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+    new_node->next = NULL;
+    memcpy(new_node->data, val, data_size);
+    if (list->head == NULL) {
+        list->head = new_node;
+    } else {
+        Node *last_node = get_end_node(list);
+        last_node->next = new_node;
+    }
+    list->size++;
 }
 
 void free_list(List *list) {
@@ -100,6 +148,7 @@ void free_list(List *list) {
 }
 
 int main() {
+    // Test assign
     int size = 12;
     int a[12];
     List *list = init_empty_list();
@@ -112,10 +161,28 @@ int main() {
         printf("Value: %d\n", *(int *)current->data);
         current = current->next;
     }
+    // Test front and back
     int *front_val = front(list);
     int *back_val = back(list);
     printf("Front: %d\n", *front_val);
     printf("Back: %d\n", *back_val);
+    // Test push_back and push_front
+    int val_back = -1;
+    push_back(list, &val_back, sizeof(int));
+    int val_front = 12;
+    push_front(list, &val_front, sizeof(int));
+
+    current = list->head;
+    while (current != NULL) {
+        printf("Value: %d\n", *(int *)current->data);
+        current = current->next;
+    }
+
+    front_val = front(list);
+    back_val = back(list);
+    printf("Front: %d\n", *front_val);
+    printf("Back: %d\n", *back_val);
+
     free_list(list);
     return 0;
 }
